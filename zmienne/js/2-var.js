9603f50d41094d99c24616cb14e35b5dbd0606c3
Kamil Predel <kamil.predel@gmail.com>
2018-02-10 16:41:02 +0100

    zmienne

---
+++ zmienne/js/2-var.js
@@ -0,0 +1,15 @@
+//var kolorStronyMojejInternetowej;
+
+var imie = "Adam";
+var nazwisko = "Nowacki";
+var wiek = 35;
+var wzrost;
+
+//var wynik = "Imię: " + imie + " Nazwisko: " + nazwisko + " Wiek: " + wiek;
+//console.log(imie);
+//console.log(nazwisko);
+//console.log(wiek);
+//console.log(wynik);
+
+console.log(wzrost);
+
