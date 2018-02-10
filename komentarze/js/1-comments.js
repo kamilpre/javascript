6a71d05378d3e62e85dc68fbb773cef29bb57f0f
Kamil Predel <kamil.predel@gmail.com>
2018-02-10 15:48:29 +0100

    komentarze

---
+++ komentarze/js/1-comments.js
@@ -0,0 +1,7 @@
+/*
+    Komentarz blokowy
+    dzieki niemu mozemy nasz komentarz umiescic w wielu liniach
+    skrók klawiszowy CTRL + SHIFT + /
+*/
+
+//komentarz liniowy, skonczy sie razem z koncem tego wiersza, skrót CTRL + /
