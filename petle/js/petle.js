8318f6225fdb99891015418ce69d670743835391
Kamil Predel <kamil.predel@gmail.com>
2018-02-11 16:37:12 +0100

    petle

---
+++ petle/js/petle.js
@@ -0,0 +1,40 @@
+//var ileRazy = 30;
+//
+//for ( var i = 0; i < ileRazy; i++ ) {
+//    var iteracja = i + 1;
+//    console.log("Iteracja pętli nr " + iteracja);
+//}
+
+//var tablica = ["Audi", "Opel", "Honda", "Mazda", "Nissan"];
+
+//for( var i = 0; i < tablica.length; i++ ) {
+//    console.log( "Tablica! Index = " + i + ", wartość = " + tablica[i] );
+//}
+
+//tablica.forEach( function( elementTablicy, indexTablicy ) {
+// if( elementTablicy == "Mazda") {
+//   console.log( "Index: " + indexTablicy + ", wartość: " + elementTablicy); 
+// }
+//}  );
+
+//for( var i = 0; i < tablica.length; i++ ) {
+//    if( i == 2 ) {
+//        continue;
+//    }
+//    
+//    console.log( i, tablica[i] );
+//}
+
+
+//var iter = 0;
+//while( iter < 20 ) {
+//    console.log( iter );
+//    ++iter;
+//}
+
+
+var iter = 20;
+do {
+    console.log(iter);
+    ++iter;
+} while( iter < 10 );
